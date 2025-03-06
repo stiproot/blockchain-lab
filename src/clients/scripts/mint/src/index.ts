f04913c1aeb36fd803a71e88f3569080f66e9fc8
stiproot <stipcich.simon@gmail.com>
2025-03-06 11:39:54 +0200

    feat: umi.

--- src/clients/scripts/mint/src/index.ts
+++ src/clients/scripts/mint/src/index.ts
@@ -2,11 +2,25 @@
 import { Keypair, Connection, PublicKey, clusterApiUrl } from '@solana/web3.js';
 import { loadWalletKeypair } from './utls';
 import { buildUmi } from './factories';
+import { createSignerFromKeypair } from '@metaplex-foundation/umi';
+import { fromWeb3JsPublicKey } from '@metaplex-foundation/umi-web3js-adapters';
 
 // Example usage
 async function main() {
 
   const umi = buildUmi();
+
+  const payer: Keypair = await loadWalletKeypair();
+
+  const umiKeypair = umi.eddsa.createKeypairFromSecretKey(payer.secretKey);
+
+  const payerSigner = createSignerFromKeypair(umi, umiKeypair);
+  // const mintAuthoritySigner = createSignerFromKeypair(umi, mintAuthority);
+
+  // umi.use(keypairIdentity(payerSigner));
+
+  // Create a new mint
+  // const mint = findMintPda(umi, { programId: TokenProgram.publicKey });
 }
 
 main().then(() => {
