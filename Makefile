6b141adefde3db208457837480c6054f85afb96c
Simon Stipcich <simon.stipcich@derivco.co.za>
2025-03-06 15:30:57 +0200

    feat: clients.

---
+++ Makefile
@@ -0,0 +1,5 @@
+download-mpl:
+	solana program dump --url mainnet-beta metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s src/programs/metadata.so
+
+install-mpl:
+	solana-test-validator --bpf-program metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s src/programs/metadata.so
--- src/clients/scripts/mint/src/index.ts
+++ src/clients/scripts/mint/src/index.ts
@@ -1,26 +1,40 @@
 // import { createTokenWithMetadata } from "./mint";
-import { Keypair, Connection, PublicKey, clusterApiUrl } from '@solana/web3.js';
+import { Keypair as Web3Keypair, Connection, PublicKey, clusterApiUrl } from '@solana/web3.js';
 import { loadWalletKeypair } from './utls';
 import { buildUmi } from './factories';
-import { createSignerFromKeypair } from '@metaplex-foundation/umi';
+import { createSignerFromKeypair, generateSigner, Keypair, keypairIdentity, percentAmount, Umi } from '@metaplex-foundation/umi';
 import { fromWeb3JsPublicKey } from '@metaplex-foundation/umi-web3js-adapters';
+import { createV1, TokenStandard } from '@metaplex-foundation/mpl-token-metadata';
+
+async function buildWalletKeypair(umi: Umi): Promise<Keypair> {
+  const payer: Web3Keypair = await loadWalletKeypair();
+  const umiKeypair = umi.eddsa.createKeypairFromSecretKey(payer.secretKey);
+  return umiKeypair;
+}
 
 // Example usage
 async function main() {
 
   const umi = buildUmi();
 
-  const payer: Keypair = await loadWalletKeypair();
+  const walletKeypair = await buildWalletKeypair(umi);
+  const payerSigner = createSignerFromKeypair(umi, walletKeypair);
+  const mintAuthoritySigner = createSignerFromKeypair(umi, walletKeypair);
 
-  const umiKeypair = umi.eddsa.createKeypairFromSecretKey(payer.secretKey);
+  umi.use(keypairIdentity(payerSigner));
 
-  const payerSigner = createSignerFromKeypair(umi, umiKeypair);
-  // const mintAuthoritySigner = createSignerFromKeypair(umi, mintAuthority);
-
-  // umi.use(keypairIdentity(payerSigner));
-
-  // Create a new mint
   // const mint = findMintPda(umi, { programId: TokenProgram.publicKey });
+
+  const mint = generateSigner(umi);
+
+  await createV1(umi, {
+    mint: mint,
+    authority: mintAuthoritySigner,
+    name: 'scorpion-nft',
+    uri: "https://en.wikipedia.org/wiki/Scorpion_(Mortal_Kombat)#/media/File:ScorpionMortalKombatx.jpg",
+    sellerFeeBasisPoints: percentAmount(5.5),
+    tokenStandard: TokenStandard.NonFungible,
+  }).sendAndConfirm(umi);
 }
 
 main().then(() => {
